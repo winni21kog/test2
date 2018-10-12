9ebdc857f51ca865152e713244061c3abb2a1a6f
winni21kog <winni21kog@gmail.com>
2018-10-12 15:07:47 +0800

    加入db

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleApp2.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,11 +9,20 @@ namespace ConsoleApp2
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             //test
+            UNEntities db = new UNEntities();
+            var q = db.Activity.Where(a => a.StatusID == "AE").ToList();
             Console.WriteLine("test");
+            for (int i = 0; i < q.Count; i++)
+            {
+                Console.WriteLine(q[i].ActName.ToString());
+            }
             Console.ReadLine();
+            
+            
 
         }
     }
